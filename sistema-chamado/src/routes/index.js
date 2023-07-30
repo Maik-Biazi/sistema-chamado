f4b45d33c07b54daa57aa99ee5f5416b6e38a396
Maik-Biazi <maikwilliam4553@gmail.com>
2023-07-30 17:27:03 -0300

    finalizando pagina de login

--- sistema-chamado/src/routes/index.js
+++ sistema-chamado/src/routes/index.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
+import Dashboard from '../pages/dashboard'
 
 
 export default function RouteApp() {
@@ -8,6 +9,7 @@ export default function RouteApp() {
         <Routes>
             <Route path="/" element={<SignIn />} />
             <Route path="/register" element={<SignUp />} />
+            <Route path='/dashboard' element={<Dashboard />} />
         </Routes>
     )
 }
