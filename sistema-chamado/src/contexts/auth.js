5cd234674d091846004dffc667ec12b37ec3271b
Maik-Biazi <maikwilliam4553@gmail.com>
2023-08-01 21:55:42 -0300

    criado cadastro de usuario

--- sistema-chamado/src/contexts/auth.js
+++ sistema-chamado/src/contexts/auth.js
@@ -1,15 +1,48 @@
-import { useContext, useState, useEffect } from 'react'
+import { useState, createContext } from 'react'
+import { auth, db } from '../services/firebaseConnection'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { getDoc, doc, setDoc } from 'firebase/firestore'
 
 
-const AuthProvider = createContext({})
+export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
     const [user, setUser] = useState(null)
+    const [loadingAuth, setLoadingAuth] = useState(false)
 
     function sigIn(email, password) {
         console.log(email)
         console.log(password)
         console.log('logado com sucesso')
+    }
+    async function signUp(email, password, name) {
+        setLoadingAuth(true)
+        await createUserWithEmailAndPassword(auth, email, password)
+            .then(async (value) => {
+                let uid = value.user.uid
+
+                await setDoc(doc(db, "users", uid), {
+                    name: name,
+                    avatarUrl: null
+                })
+                    .then(() => {
+                        let data = {
+                            uid: uid,
+                            name: name,
+                            email: value.user.email,
+                            avatarUrl: null
+                        }
+                        setUser(data)
+                        setLoadingAuth(false)
+                    })
+
+
+            })
+            .catch((error) => {
+                console.log(error)
+                setLoadingAuth(false)
+            })
+
     }
 
     return (
@@ -17,7 +50,9 @@ function AuthProvider({ children }) {
             value={{
                 signed: !!user,
                 user,
-                sigIn
+                sigIn,
+                signUp,
+                loadingAuth
             }}>
             {children}
         </AuthContext.Provider>
