a810dcde6e7a9dc0385d344c4ae8f621a112dc8c
Maik-Biazi <maikwilliam4553@gmail.com>
2023-08-02 23:19:22 -0300

    aplicando toast

--- sistema-chamado/src/App.js
+++ sistema-chamado/src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter } from 'react-router-dom'
 import RouteApp from './routes';
 import AuthProvider from './contexts/auth';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify'
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
+        <ToastContainer autoClose={3000} />
         <RouteApp />
       </AuthProvider>
     </BrowserRouter>
--- sistema-chamado/src/contexts/auth.js
+++ sistema-chamado/src/contexts/auth.js
@@ -2,6 +2,9 @@ import { useState, createContext } from 'react'
 import { auth, db } from '../services/firebaseConnection'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { getDoc, doc, setDoc } from 'firebase/firestore'
+import { toast } from 'react-toastify'
+
+import { useNavigate } from 'react-router-dom'
 
 
 export const AuthContext = createContext({})
@@ -10,39 +13,81 @@ function AuthProvider({ children }) {
     const [user, setUser] = useState(null)
     const [loadingAuth, setLoadingAuth] = useState(false)
 
+    const navigate = useNavigate()
+
     function sigIn(email, password) {
         console.log(email)
         console.log(password)
         console.log('logado com sucesso')
     }
+    // async function signUp(email, password, name) {
+    //     setLoadingAuth(true)
+
+    //     await createUserWithEmailAndPassword(auth, email, password)
+    //         .then(async (value) => {
+    //             let uid = value.user.uid
+
+
+    //             await setDoc(doc(db, "users", uid), {
+    //                 name: name,
+    //                 avatarUrl: null
+    //             })
+    //                 .then(() => {
+    //                     const token = user.getIdToken(); // Get the JWT token
+    //                     let data = {
+
+    //                         uid: uid,
+    //                         name: name,
+    //                         email: value.user.email,
+    //                         avatarUrl: null,
+    //                         token
+    //                     }
+    //                     setUser(data)
+    //                     storageUser(data)
+    //                     setLoadingAuth(false)
+    //                     navigate("/dashboard")
+    //                 })
+
+
+    //         })
+    //         .catch((error) => {
+    //             console.log(error)
+    //             setLoadingAuth(false)
+    //         })
+
+    // }
+
     async function signUp(email, password, name) {
-        setLoadingAuth(true)
-        await createUserWithEmailAndPassword(auth, email, password)
-            .then(async (value) => {
-                let uid = value.user.uid
+        setLoadingAuth(true);
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+            const token = await user.getIdToken(); // Get the JWT token
+            const userData = {
+                uid: user.uid,
+                email: user.email,
+                name: name,
+                avatarUrl: null,
+                token, // Store the JWT token in the user data
+            };
 
-                await setDoc(doc(db, "users", uid), {
-                    name: name,
-                    avatarUrl: null
-                })
-                    .then(() => {
-                        let data = {
-                            uid: uid,
-                            name: name,
-                            email: value.user.email,
-                            avatarUrl: null
-                        }
-                        setUser(data)
-                        setLoadingAuth(false)
-                    })
+            await setDoc(doc(db, 'users', user.uid), {
+                name: name,
+                avatarUrl: null,
+            });
 
-
-            })
-            .catch((error) => {
-                console.log(error)
-                setLoadingAuth(false)
-            })
-
+            setUser(userData);
+            storageUser(userData);
+            setLoadingAuth(false);
+            toast.success(`seja Bem vindo ao sistema ${userData.name}`)
+            navigate('/dashboard');
+        } catch (error) {
+            console.log(error);
+            setLoadingAuth(false);
+        }
+    }
+    function storageUser(data) {
+        localStorage.setItem('@ticketsPRO', JSON.stringify(data))
     }
 
     return (
