b9ee2d04644b70ae4ee4b55e07b5c4840a427369
Maik-Biazi <maikwilliam4553@gmail.com>
2023-08-04 23:13:41 -0300

    finalizando header

--- sistema-chamado/src/pages/dashboard/index.js
+++ sistema-chamado/src/pages/dashboard/index.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import { AuthContext } from '../../contexts/auth'
+import Header from '../../components/header';
 export default function Dashboard() {
     const { logout } = useContext(AuthContext);
 
@@ -9,7 +10,9 @@ export default function Dashboard() {
 
     return (
         <div>
+            <Header />
             <h1>Sair da conta</h1>
+
             <button onClick={handleLogout}>Sair da conta</button>
         </div>
     )
