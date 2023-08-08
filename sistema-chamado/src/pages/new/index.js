2696aaa66ff481565bb7685a638fcf8202df8ce9
Maik-Biazi <maikwilliam4553@gmail.com>
2023-08-07 22:25:53 -0300

    contruindo novo chamado

--- sistema-chamado/src/pages/new/index.js
+++ sistema-chamado/src/pages/new/index.js
@@ -12,7 +12,25 @@ export default function New() {
                 <Title name='Novo chamado'>
                     <FiPlusCircle size={25} />
                 </Title>
+                <div className="container">
+                    <form
+                        className="form-profile">
+                        <label>Clientes</label>
+                        <select >
+                            <option key={1} value={1}>Mercado Teste</option>
+                            <option key={2} value={2}>Mercado Teste</option>
+                            <option key={3} value={3}>Mercado Teste</option>
+                            <option key={4} value={4}>Mercado Teste</option>
+                        </select>
+                        <label>Assunto</label>
+                        <select >
+                            <option key={1} value="suporte">Suporte</option>
+                            <option key={2} value="Visita Tecnica">Visita Tecnica</option>
+                            <option key={3} value="Financeiro">Financeiro</option>
+                        </select>
+                    </form>
 
+                </div>
             </div>
         </div>
     )
